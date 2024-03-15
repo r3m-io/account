1da6283d029a9781ce236300e6b63425e50a5ad4
remco van der Velde <remco.pc@outlook.com>
2024-03-15 20:15:26 +0100

    init account

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -20,25 +20,36 @@ trait Main {
         $object = $this->object();
         $url = $object->config('project.dir.data') . 'Account/Role.System.json';
         $data = $object->data_read($url);
+        $node = new Node($object);
+        $role = $node->role_system();
         if($data){
             $permissions = $data->get('permission');
             if(is_array($permissions)){
-                $node = new Node($object);
+
                 $name = 'Account.Permission';
                 $options_list = $options;
                 if(!property_exists($options_list, 'limit')){
                     $options_list->limit = '*';
+                    $options_list->page = 1;
                 }
-                $response = $node->list($name, $node->role_system(), $options);
-                ddd($response);
-
-
-
-//                $node->import($name, $node->role_system(), $options);
+                $response = $node->list($name, $role, $options);
+                if($response){
+                    if(array_key_exists('list', $response)){
+                        $list = $response['list'];
+                        foreach($list as $item){
+                            ddd($item);
+                        }
+                    }
+                    $create_many = $permissions;
+                    $response = $node->create_many($name, $role, $create_many, [
+                        'import' => true,
+                        'uuid' => false,
+                        'validation' => $options['validation'] ?? true
+                    ]);
+                    d($response);
+                }
             }
-            ddd($permissions);
         }
-        ddd($data);
 
         d($flags);
         d($options);
