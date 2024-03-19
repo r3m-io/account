7775796eee17b66c643c86831abe30ec1fc4ce17
remco van der Velde <remco.pc@outlook.com>
2024-03-19 19:17:29 +0100

    init accounts ROLE_ADMIN, ROLE_SYSTEM

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -186,7 +186,7 @@ trait Main {
                                 ' -uuid=' . $record['node']->uuid .
                                 ' -name=' . $roles_role['name'] .
                                 ' -rank=' . $roles_role['rank'] .
-                                ' -permission[]=\'' . $roles_role['permission'] . '\''
+                                ' -permission=' . $roles_role['permission']
                             ;
                             echo $command . PHP_EOL;
                             exec($command, $output);
@@ -201,8 +201,7 @@ trait Main {
                                 ' -uuid=' . $record['node']->uuid .
                                 ' -name=' . $roles_role['name'] .
                                 ' -rank=' . $roles_role['rank'] .
-                                ' -permission=[]' .
-                                ' -permission[]=\'' . $roles_role['permission'] . '\''
+                                ' -permission=' . $roles_role['permission']
                             ;
                             echo $command . PHP_EOL;
                             exec($command, $output);
@@ -216,7 +215,7 @@ trait Main {
                             ' -class=Account.Role' .
                             ' -name=' . $roles_role['name'] .
                             ' -rank=' . $roles_role['rank'] .
-                            ' -permission[]= \'' . $roles_role['permission'] . '\''
+                            ' -permission=' . $roles_role['permission']
                         ;
                         echo $command . PHP_EOL;
                         exec($command, $output);
