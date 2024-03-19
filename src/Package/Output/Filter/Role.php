fb69bd41629e657fde0d742eca456ed7e6938662
remco van der Velde <remco.pc@outlook.com>
2024-03-19 22:24:18 +0100

    multiple permission

--- src/Package/Output/Filter/Role.php
+++ src/Package/Output/Filter/Role.php
@@ -17,39 +17,15 @@ class Role extends Controller {
             !empty($response) &&
             is_array($response)
         ){
-            ddd($response);
-            foreach($response as $nr => $record){
-                if(
-                    is_array($record) &&
-                    array_key_exists('name', $record) &&
-                    array_key_exists('environment', $record)
-                ){
-                    if($record['environment'] === '*'){
-                        $result[$record['name']] = $record;
-                    } else {
-                        if(!array_key_exists($record['name'], $result)){
-                            $result[$record['name']] = [];
-                        }
-                        $result[$record['name']][$record['environment']] = $record;
-                    }
-                }
-                elseif(
+            foreach($response as $nr => $record){if(
                     is_object($record) &&
-                    property_exists($record, 'name') &&
-                    property_exists($record, 'environment')
+                    property_exists($record, 'name')
                 ){
-                    if($record->environment === '*'){
-                        $result[$record->name] = $record;
-                    } else {
-                        if(!array_key_exists($record->name, $result)){
-                            $result[$record->name] = [];
-                        }
-                        $result[$record->name][$record->environment] = $record;
-                    }
+                    $result[] = $record->name;
                 }
             }
         }
-        return (object) $result;
+        return $result;
     }
 
 }
