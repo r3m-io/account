360db8ada3c45661c7aa21fcb2cf1fc42305ac72
remco van der Velde <remco.pc@outlook.com>
2024-07-10 00:13:06 +0200

    main

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -386,7 +386,7 @@ trait Main {
     /**
      * @throws Exception
      */
-    public function user_create_login($flags, $options): bool | int
+    public function user_create_login($flags, $options): bool | array
     {
         $object = $this->object();
         if(!property_exists($options, 'namespace')){
