0199352d8e1160fc0213140af32243aa62dc7172
remco van der Velde <remco.pc@outlook.com>
2024-07-26 15:04:15 +0200

    setup user login

--- src/Package/Output/Filter/Role.php
+++ src/Package/Output/Filter/Role.php
@@ -13,20 +13,24 @@ class Role extends Controller {
     public static function permission(App $object, $response=null): array
     {
         $result = [];
+        $is_result = false;
         if(
             !empty($response) &&
             is_array($response)
         ){
-            d($response);
             foreach($response as $nr => $record){
                 if(
                     is_object($record) &&
                     property_exists($record, 'name')
                 ){
+                    $is_result = true;
                     $result[] = $record->name;
                 }
             }
         }
-        return $result;
+        if($is_result){
+            return $result;
+        }
+        return $response;
     }
 }
