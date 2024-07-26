863e19ce4465d1c22671f5bff2e36a91e5c3f1ff
remco van der Velde <remco.pc@outlook.com>
2024-07-26 15:47:54 +0200

    setup user login

--- src/Package/Output/Filter/Role.php
+++ src/Package/Output/Filter/Role.php
@@ -18,21 +18,8 @@ class Role extends Controller {
 
     public static function user(App $object, $response=null): array
     {
-        //permission array can be compacted to array
-        if(is_array($response)){
-            foreach($response as $nr => $role){
-                if(
-                    property_exists($role, 'permission') &&
-                    is_array($role->permission)
-                ){
-                    foreach($role->permission as $permission_nr => $permission){
-                        if(property_exists($permission, 'name')){
-                            $role->permission[$permission_nr] = $permission->name;
-                        }
-                    }
-                }
-            }
-        }
+        //permission array should stay intact
         return $response;
     }
+
 }
