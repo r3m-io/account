10ff92f17e2e309145936b6e96ea0d2ffb1dc682
remco van der Velde <remco.pc@outlook.com>
2024-07-26 15:22:12 +0200

    setup user login

--- src/Package/Output/Filter/Role.php
+++ src/Package/Output/Filter/Role.php
@@ -12,26 +12,7 @@ class Role extends Controller {
 
     public static function permission(App $object, $response=null): array
     {
-        return $response;
-        $result = [];
-        $is_result = false;
-        if(
-            !empty($response) &&
-            is_array($response)
-        ){
-            foreach($response as $nr => $record){
-                if(
-                    is_object($record) &&
-                    property_exists($record, 'name')
-                ){
-                    $is_result = true;
-                    $result[] = $record->name;
-                }
-            }
-        }
-        if($is_result){
-            return $result;
-        }
+        //permission object should stay intact
         return $response;
     }
 }
